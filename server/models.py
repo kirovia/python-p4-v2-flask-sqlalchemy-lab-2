5f57f9cc697fcaba133e4f2251e4f6e976bb4997
Joseph Andrew Phillips <josephandrewphillips@gmail.com>
2025-10-15 23:07:44 -0400

    pass serialization_test.py

--- server/models.py
+++ server/models.py
@@ -11,8 +11,10 @@ metadata = MetaData(naming_convention={
 db = SQLAlchemy(metadata=metadata)
 
 
-class Customer(db.Model):
+class Customer(db.Model, SerializerMixin):
     __tablename__ = 'customers'
+
+    serialize_rules = ('-reviews.customer',)
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
@@ -25,8 +27,10 @@ class Customer(db.Model):
         return f'<Customer {self.id}, {self.name}>'
 
 
-class Item(db.Model):
+class Item(db.Model, SerializerMixin):
     __tablename__ = 'items'
+
+    serialize_rules = ('-reviews.item',)
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
@@ -37,8 +41,10 @@ class Item(db.Model):
     def __repr__(self):
         return f'<Item {self.id}, {self.name}, {self.price}>'
 
-class Review(db.Model):
+class Review(db.Model, SerializerMixin):
     __tablename__ = 'reviews'
+
+    serialize_rules = ('-customer.reviews', '-item.reviews',)
 
     id = db.Column(db.Integer, primary_key=True)
     comment = db.Column(db.String)
